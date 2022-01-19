ba7cf9f9652146df52fc78e7bbad8c312c14abf3
Berqia Mouad <83679093+BerqiaMouad@users.noreply.github.com>
2022-01-19 19:55:48 +0100

    Add files via upload

---
+++ js/navbar.js
@@ -0,0 +1,37 @@
+const container = document.querySelector('.page-container');
+const pages = [...document.querySelectorAll('.page')];
+const toggleBtn = document.querySelector('.toggle-btn');
+const ul = document.querySelector('.nav-list');
+const overlay = document.querySelector('.overlay');
+const links = [...document.querySelectorAll('.link')];
+const goToBtn = document.querySelector('.btn')
+
+let currentPageIndex = 0;
+
+toggleBtn.addEventListener('click', () => {
+    toggleBtn.classList.toggle('active');
+    container.classList.toggle('active');
+    ul.classList.toggle('show');
+})
+
+const changePage = (i) => {
+    overlay.style.animation = `slide 1s linear 1`;
+    setTimeout(() => {
+        pages[currentPageIndex].classList.remove('active');
+        pages[i].classList.add('active');
+        currentPageIndex = i;
+    }, 500);
+    setTimeout(() => {
+        overlay.style.animation = null;
+    }, 1000);
+}
+
+goToBtn.addEventListener('click', () => {
+    changePage(1);
+})
+
+links.forEach((item, i) => {
+    item.addEventListener('click', () => {
+        changePage(i);
+    })
+})
