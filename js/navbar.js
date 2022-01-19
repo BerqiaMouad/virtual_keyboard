08c08e6fefb30fbc6b10e97f3b22017fb8638574
Berqia Mouad <83679093+BerqiaMouad@users.noreply.github.com>
2022-01-19 19:52:43 +0100

    Delete js directory

--- js/navbar.js
+++
@@ -1,6 +0,0 @@
-const my_menu = document.querySelector(".my-menu");
-const container = document.querySelector(".container");
-
-my_menu.addEventListener("click", () => {
-    container.classList.toggle("active");
-})
